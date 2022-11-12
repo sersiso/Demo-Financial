63d8e91974d71347d747d1612d41d948600682c1
sergioss <sersiso82@gmail.com>
2022-11-12 11:09:03 +0100

    principaladaptada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     RouterModule.forRoot( routes, { onSameUrlNavigation: 'reload' } )
   ],
   exports: [
-    RouterModule
+    RouterModule,
   ]
 })
 export class AppRoutingModule { }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  rutaImg:string = 'url("../../assets/img/background-slider.jpg")';
+  rutaImg:string = '../../assets/img/background-slider.jpg';
+  rutaImgMovil:string = '../../assets/img/background-slider-m.jpg';
+  movil:boolean = false;
+  screenSize:string = '710';
 
-  constructor() { }
+  constructor( public breakpointObserver: BreakpointObserver ) {
+
+    this.sizeScreen();
+
+   }
 
   ngOnInit(): void {
+  }
+
+
+  sizeScreen(){
+    this.breakpointObserver
+    .observe( [`(max-width: ${ this.screenSize }px)`] )
+    .subscribe( ( state:BreakpointState )=>{
+      if ( state.matches ){
+        this.movil = true;
+      } else {
+        this.movil = false;
+      }
+    });
   }
 
 }
